9e1abf695465bc611e0eaef73a4daec2561d70d4
Aliese-30326 <wxxwsj@163.com>
2025-05-28 12:31:29 +0800

    Update workflow_download.py

--- usage/workflow_download.py
+++ usage/workflow_download.py
@@ -2,69 +2,7 @@ from jmcomic import *
 from jmcomic.cl import JmcomicUI
 
 # 下方填入你要下载的本子的id，一行一个，每行的首尾可以有空白字符
-jm_albums = '''
-
-
-
-'''
-
-# 单独下载章节
-jm_photos = '''
-
-
-
-'''
-
-
-def env(name, default, trim=('[]', '""', "''")):
-    import os
-    value = os.getenv(name, None)
-    if value is None or value == '':
-        return default
-
-    for pair in trim:
-        if value.startswith(pair[0]) and value.endswith(pair[1]):
-            value = value[1:-1]
-
-    return value
-
-
-def get_id_set(env_name, given):
-    aid_set = set()
-    for text in [
-        given,
-        (env(env_name, '')).replace('-', '\n'),
-    ]:
-        aid_set.update(str_to_set(text))
-
-    return aid_set
-
-
-def main():
-    album_id_set = get_id_set('JM_ALBUM_IDS', jm_albums)
-    photo_id_set = get_id_set('JM_PHOTO_IDS', jm_photos)
-
-    helper = JmcomicUI()
-    helper.album_id_list = list(album_id_set)
-    helper.photo_id_list = list(photo_id_set)
-
-    option = get_option()
-    helper.run(option)
-    option.call_all_plugin('after_download')
-
-
-def get_option():
-    # 读取 option 配置文件
-    option = create_option(os.path.abspath(os.path.join(__file__, '../../assets/option/option_workflow_download.yml')))
-
-    # 支持工作流覆盖配置文件的配置
-    cover_option_config(option)
-
-    # 把请求错误的html下载到文件，方便GitHub Actions下载查看日志
-    log_before_raise()
-
-    return option
-jm_albums = str_to_lost('''
+jm_albums = ('''
 jm54747
 jm28650
 jm27512
@@ -231,7 +169,7 @@ jm409658
 jm116157 
 jm395281 
 jm395280
-jm392326
+jm392326从
 jm469738
 jm537119
 jm542252
@@ -353,6 +291,63 @@ jm373099
 jm138809
 
 ''')
+
+# 单独下载章节
+jm_photos = '''
+
+
+
+'''
+
+
+def env(name, default, trim=('[]', '""', "''")):
+    import os
+    value = os.getenv(name, None)
+    if value is None or value == '':
+        return default
+
+    for pair in trim:
+        if value.startswith(pair[0]) and value.endswith(pair[1]):
+            value = value[1:-1]
+
+    return value
+
+
+def get_id_set(env_name, given):
+    aid_set = set()
+    for text in [
+        given,
+        (env(env_name, '')).replace('-', '\n'),
+    ]:
+        aid_set.update(str_to_set(text))
+
+    return aid_set
+
+
+def main():
+    album_id_set = get_id_set('JM_ALBUM_IDS', jm_albums)
+    photo_id_set = get_id_set('JM_PHOTO_IDS', jm_photos)
+
+    helper = JmcomicUI()
+    helper.album_id_list = list(album_id_set)
+    helper.photo_id_list = list(photo_id_set)
+
+    option = get_option()
+    helper.run(option)
+    option.call_all_plugin('after_download')
+
+
+def get_option():
+    # 读取 option 配置文件
+    option = create_option(os.path.abspath(os.path.join(__file__, '../../assets/option/option_workflow_download.yml')))
+
+    # 支持工作流覆盖配置文件的配置
+    cover_option_config(option)
+
+    # 把请求错误的html下载到文件，方便GitHub Actions下载查看日志
+    log_before_raise()
+
+    return option
 
 def cover_option_config(option: JmOption):
     dir_rule = env('DIR_RULE', None)
